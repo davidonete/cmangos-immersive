b1eec715725a57e936e2f3dbd64f0e69d9136676
David <davidparraausina@gmail.com>
2024-01-26 12:56:41 +0000

    Change gossip to id 19

--- sql/world/world.sql
+++ sql/world/world.sql
@@ -35,42 +35,42 @@ VALUES (60003, @TEXT_ID+2, 0, 0);
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 0, 7, 'Check Current Attributes', 18, 16, 0, 0, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 0, 7, 'Check Current Attributes', 19, 16, 0, 0, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 0, 'Ver Atributos Actuales', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 1, 3, 'Improve Strength', 18, 16, 0, 1, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 1, 3, 'Improve Strength', 19, 16, 0, 1, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 1, 'Mejorar Fuerza', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 2, 3, 'Improve Agility', 18, 16, 0, 2, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 2, 3, 'Improve Agility', 19, 16, 0, 2, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 2, 'Mejorar Agilidad', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 3, 3, 'Improve Stamina', 18, 16, 0, 3, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 3, 3, 'Improve Stamina', 19, 16, 0, 3, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 3, 'Mejorar Aguante', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 4, 3, 'Improve Intellect', 18, 16, 0, 4, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 4, 3, 'Improve Intellect', 19, 16, 0, 4, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 4, 'Mejorar Intelecto', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60001, 5, 3, 'Improve Spirit', 18, 16, 0, 5, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60001, 5, 3, 'Improve Spirit', 19, 16, 0, 5, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60001, 5, 'Mejorar Espiritu', '');
@@ -84,7 +84,7 @@ VALUES (60001, 7, 'Reiniciar todos los atributos', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60002, 6, 4, 'I am sure I do want to unlearn all attributes', 18, 16, 0, 6, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60002, 6, 4, 'I am sure I do want to unlearn all attributes', 19, 16, 0, 6, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60002, 6, 'Estoy seguro que quiero reiniciar mis atributos', '');
@@ -98,49 +98,49 @@ VALUES (60001, 8, 'Reducir los atributos temporalmente', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 11, 3, 'Remove Reduction', 18, 16, 0, 11, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 11, 3, 'Remove Reduction', 19, 16, 0, 11, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 11, 'Quitar reducción de atributos', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 12, 3, 'Reduce by 90%', 18, 16, 0, 12, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 12, 3, 'Reduce by 90%', 19, 16, 0, 12, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 12, 'Reducir un 90%', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 14, 3, 'Reduce by 70%', 18, 16, 0, 14, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 14, 3, 'Reduce by 70%', 19, 16, 0, 14, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 14, 'Reducir un 70%', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 16, 3, 'Reduce by 50%', 18, 16, 0, 16, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 16, 3, 'Reduce by 50%', 19, 16, 0, 16, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 16, 'Reducir un 50%', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 17, 3, 'Reduce by 30%', 18, 16, 0, 18, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 17, 3, 'Reduce by 30%', 19, 16, 0, 18, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 17, 'Reducir un 30%', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 18, 3, 'Reduce by 20%', 18, 16, 0, 18, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 18, 3, 'Reduce by 20%', 19, 16, 0, 18, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 18, 'Reducir un 20%', '');
 
 INSERT INTO `gossip_menu_option`
 (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`,
-`box_money`, `box_text`, `condition_id`) VALUES (60003, 19, 3, 'Reduce by 10%', 18, 16, 0, 20, 0, 0, 0, '', 0);
+`box_money`, `box_text`, `condition_id`) VALUES (60003, 19, 3, 'Reduce by 10%', 19, 16, 0, 20, 0, 0, 0, '', 0);
 
 INSERT INTO `locales_gossip_menu_option` (`menu_id`, `id`, `option_text_loc6`, `box_text_loc6`) 
 VALUES (60003, 19, 'Reducir un 10%', '');
